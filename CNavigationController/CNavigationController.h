af142afeaf4346022c91cc7886b21f6e53955a53
Anton Paliakou <toxa95401@gmail.com>
2021-08-06 11:29:30 +0300

    Add base framework.

---
+++ CNavigationController/CNavigationController.h
@@ -0,0 +1,18 @@
+//
+//  CNavigationController.h
+//  CNavigationController
+//
+//  Created by Anton Paliakou on 8/6/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CNavigationController.
+FOUNDATION_EXPORT double CNavigationControllerVersionNumber;
+
+//! Project version string for CNavigationController.
+FOUNDATION_EXPORT const unsigned char CNavigationControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CNavigationController/PublicHeader.h>
+
+
